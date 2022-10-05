fa6e05f06d1b3228629268d01275e0fee90cd822
Sumeyye Guler <sumeyyeguler0602@gmail.com>
2022-10-05 15:09:55 +0300

    "5.10.2022"

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -4,71 +4,66 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.opera.OperaDriver;
-import org.openqa.selenium.safari.SafariDriver;
 
 import java.time.Duration;
 
 public class Driver {
-    /*
-    POM'de Driver icin TestBase class'ina extends etmek yerine
-    Driver class'indan static method'lar kullanarak
-    driver olusturup, ilgili ayarlarin yapilmasi
-    ve en sonda driver'in kapatilmasi tercih edilmistir.
-    POM'de Driver class'indaki getDriver()'nin obje olusturularak kullanilmasini
-    engellemek icin
-    Singleton pattern kullanimi benimsenmistir.
-    Singleton Pattern : tekli kullanim, bir class'in farkli class'lardan
-    obje olusturularak kullanimini engellemek icin kullanilir.
-    Bunu saglamak icin yapmamiz gereken sey oldukca basit
-    obje olusturmak icin kullanilan constructor'i private yaptiginizda
-    bsaka class'larda Driver class'indan obje olusturulmasi mumkun OLAMAZ
-     */
-    private  Driver(){
+
+    private Driver(){
 
     }
+
     static WebDriver driver;
 
-    public static WebDriver getDriver(){
+        /*
+        Testlerimizi çalıştırdığımızda her seferinde yeni driver oluşturduğu için her test methodu
+        için yeni bir pencere(driver) açıyor. Eğer driver'a bir değer atanmamışsa yani driver==null ise
+        bir kere driver'i çalıştır diyerek bir kere if içini çalıştıracak. Ve driver artık bir kere
+        çalıştığı için ve değer atandığı için null olmayacak ve direk return edecek ve diğer
+        teslerimiz aynı pencere(driver) üzerinde çalışacak
+         */
 
-        if (driver==null) {
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
             switch (ConfigReader.getProperty("browser")){
-                case "chrome" :
-                    WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver();
-                    break;
-                case "safari" :
-                    WebDriverManager.safaridriver().setup();
-                    driver=new SafariDriver();
+                case "edge" :
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
                     break;
                 case "firefox" :
                     WebDriverManager.firefoxdriver().setup();
-                    driver=new FirefoxDriver();
+                    driver = new FirefoxDriver();
                     break;
-                case "headless-chrome": //chrome driver görünmez olarak açılıyor
+                case "opera" :
+                    WebDriverManager.operadriver().setup();
+                    driver = new OperaDriver();
+                    break;
+                case "headless-chrome" :
                     WebDriverManager.chromedriver().setup();
                     driver = new ChromeDriver(new ChromeOptions().setHeadless(true));
                     break;
                 default:
                     WebDriverManager.chromedriver().setup();
                     driver = new ChromeDriver();
-
             }
-
-
             driver.manage().window().maximize();
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         }
         return driver;
     }
-
-    public static void closeDriver(){
-        if (driver!=null){ // driver'a deger atanmissa
+    public static void closeDriver() {
+        if (driver != null) { // driver'a değer atanmışsa kapat
             driver.close();
-            driver=null;
+            driver = null; // Kapandıktan sonra sonraki açmaları garanti altına almak için driver'i tekrar null yaptık
         }
-
     }
-
+    public static void quitDriver() {
+        if (driver != null)
+            driver.quit();
+        driver = null;
+    }
 }
